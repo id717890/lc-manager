7ac3e051c732c05217d9050ebc72bc29467b0da9
YusupovZA <jusupovz@gmail.com>
2018-02-27 10:34:49 +0500

    JWT in WebAPI

--- LCManagerClient/LCManagerPartner/Startup.cs
+++ LCManagerClient/LCManagerPartner/Startup.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Configuration;
 using System.Threading.Tasks;
 using Microsoft.Owin;
 using Owin;
 using Microsoft.Owin.Security.OAuth;
 using System.Web.Http;
+using LCManagerPartner.Implementation;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+using Microsoft.Owin.Security.Jwt;
 
 [assembly: OwinStartup(typeof(LCManagerPartner.Startup))]
 
@@ -13,6 +18,19 @@ namespace LCManagerPartner
     {
         public void Configuration(IAppBuilder app)
         {
+            var issuer = ConfigurationManager.AppSettings["issuer"];
+            var secret = TextEncodings.Base64Url.Decode(ConfigurationManager.AppSettings["secret"]);
+
+            app.UseJwtBearerAuthentication(new JwtBearerAuthenticationOptions
+            {
+                AuthenticationMode = AuthenticationMode.Active,
+                AllowedAudiences = new[] { ConfigurationManager.AppSettings["audience"] },
+                IssuerSecurityTokenProviders = new IIssuerSecurityTokenProvider[]
+                {
+                    new SymmetricKeyIssuerSecurityTokenProvider(issuer, secret)
+                }
+            });
+
             // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
             //enable cors origin requests
             app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
@@ -23,7 +41,8 @@ namespace LCManagerPartner
                 AllowInsecureHttp = true,
                 TokenEndpointPath = new PathString("/ManagerLogin"),
                 AccessTokenExpireTimeSpan = TimeSpan.FromDays(365),
-                Provider = myProvider
+                Provider = myProvider,
+                AccessTokenFormat = new Implementation.JwtFormat(issuer)
             };
             app.UseOAuthAuthorizationServer(options);
             app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
