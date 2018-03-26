1f116fb056cc0dbfecab190d93f26a966d646ecf
YusupovZA <jusupovz@gmail.com>
2018-03-26 10:39:41 +0500

    refresh_token через ConcurrentDictionary

---
+++ LCManagerClient/LCManagerPartner/AuthRefreshTokenProvider.cs
@@ -0,0 +1,63 @@
+﻿namespace LCManagerPartner
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Threading.Tasks;
+    using Microsoft.Owin.Security;
+    using Microsoft.Owin.Security.Infrastructure;
+
+    /// <summary>
+    /// Провайдер обеспечивающий генерацию refresh_token
+    /// </summary>
+    public class AuthRefreshTokenProvider : IAuthenticationTokenProvider
+    {
+        private static ConcurrentDictionary<string, AuthenticationTicket> _refreshTokens = new ConcurrentDictionary<string, AuthenticationTicket>();
+
+        public async Task CreateAsync(AuthenticationTokenCreateContext context)
+        {
+            var guid = Guid.NewGuid().ToString();
+
+            // maybe only create a handle the first time, then re-use for same client
+            // copy properties and set the desired lifetime of refresh token
+            var refreshTokenProperties = new AuthenticationProperties(context.Ticket.Properties.Dictionary)
+            {
+                IssuedUtc = context.Ticket.Properties.IssuedUtc,
+                ExpiresUtc = DateTime.UtcNow.AddYears(1)
+            };
+            var refreshTokenTicket = new AuthenticationTicket(context.Ticket.Identity, refreshTokenProperties);
+
+            //_refreshTokens.TryAdd(guid, context.Ticket);
+            _refreshTokens.TryAdd(guid, refreshTokenTicket);
+
+            // consider storing only the hash of the handle
+            context.SetToken(guid);
+        }
+
+        /// <summary>
+        /// Извлекает токен из хранилища, если он есть
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task ReceiveAsync(AuthenticationTokenReceiveContext context)
+        {
+            //var allowedOrigin = context.OwinContext.Get<string>("as:clientAllowedOrigin");
+            //context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { allowedOrigin });
+
+            AuthenticationTicket ticket;
+            if (_refreshTokens.TryRemove(context.Token, out ticket))
+            {
+                context.SetTicket(ticket);
+            }
+        }
+
+        public void Create(AuthenticationTokenCreateContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Receive(AuthenticationTokenReceiveContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- LCManagerClient/LCManagerPartner/Implementation/JwtFormat.cs
+++ LCManagerClient/LCManagerPartner/Implementation/JwtFormat.cs
@@ -26,6 +26,8 @@
             var signingKey = new HmacSigningCredentials(Secret);
             var issued = data.Properties.IssuedUtc;
             var expires = data.Properties.ExpiresUtc;
+            //var expires = issued.Value.DateTime.AddMinutes(Convert.ToInt32(ConfigurationManager.AppSettings["expiration_jwt_token"]));
+            //var expires2 = DateTimeOffset.UtcNow.AddMinutes(Convert.ToInt32(ConfigurationManager.AppSettings["expiration_jwt_token"]));
 
             return new JwtSecurityTokenHandler().WriteToken(
                 new JwtSecurityToken(
