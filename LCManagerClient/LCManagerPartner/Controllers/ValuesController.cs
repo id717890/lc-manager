b92c8b04855cc38436ab1c7487242934334349eb
Alexander Smirnov <aleksandr.smirnov2009@gmail.com>
2018-06-21 19:47:37 +0200

    expanded logs to refund

--- LCManagerClient/LCManagerPartner/Controllers/ValuesController.cs
+++ LCManagerClient/LCManagerPartner/Controllers/ValuesController.cs
@@ -253,7 +253,13 @@
         [Route("Refund")]
         public RefundResponse Refund(RefundRequest request)
         {
-            Log.Information("LCManagerPartner Refund {Phone}", request.Phone);
+            //Log.Information("LCManagerPartner Refund {Phone}", request.Phone);
+            Log.Information("LCManagerPartner GetRegistrationUser Operator = {Operator}, Phone = {phone}, Amount = {Amount}, Card = {Card}, ChequeTime = {ChequeTime}, " +
+                                        "Number = {Number}, PaidByBonus = {PaidByBonus}, Partner = {Partner}, Pos = {Pos}, PurchaseDate = {PurchaseDate}, PurchaseId = {PurchaseId}, " +
+                                        "PurchaseNumber = {PurchaseNumber}, PurchasePos = {PurchasePos}, PurchaseTerminal = {PurchaseTerminal}",
+                                                            request.Operator, request.Phone, request.Amount, request.Card, request.ChequeTime,
+                                                            request.Number, request.PaidByBonus, request.Partner, request.Pos, request.PurchaseDate, request.PurchaseId, request.PurchaseNumber,
+                                                            request.PurchasePos, request.PurchaseTerminal);
             var result = new ServerRefundResponse();
             var returnValue = result.ProcessRequest(cnn, request);
             return returnValue;
