c494f5b50666010162f71a9bf234c4914159964d
YusupovZA <jusupovz@gmail.com>
2018-06-26 01:16:41 +0300

    ДОБАВИЛ СБОРКУ JWT, подключил бонусы не за покупки

---
+++ LCManagerClient/LCManager.Jwt/JwtProps.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using Site.Infrastrucure;
+
+namespace LCManager.JWT
+{
+    public class JwtProps
+    {
+        #region Public methods
+
+        /// <summary>
+        /// Получает payload из токена
+        /// </summary>
+        /// <returns></returns>
+        public static dynamic GetPayloadFromToken()
+        {
+            return !IsExistTokenInCookie() ? null : JwtProvider.GetInstance().DecodePayload(ReadJwtTokenFromCookie().Value);
+        }
+
+        /// <summary>
+        /// Получает токен из куков
+        /// </summary>
+        /// <returns></returns>
+        public static string GetToken()
+        {
+            return !IsExistTokenInCookie() ? string.Empty : HttpContext.Current.Request.Cookies[Config.GetNameCookieOfAccessToken()].Value;
+        }
+
+        /// <summary>
+        /// Получает refresh_token из куков браузера
+        /// </summary>
+        /// <returns></returns>
+        public static string GetRefreshToken()
+        {
+            var cookie = HttpContext.Current.Request.Cookies[Config.GetNameCookieOfRefreshToken()];
+            return cookie?.Value;
+        }
+
+        /// <summary>
+        /// Проверяем есть ли token в куках
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsExistTokenInCookie()
+        {
+            //var cookie = new HttpCookie("lcmanageruserdata")
+            //{
+            //    Expires = DateTime.Now.AddDays(-1d)
+            //};
+            //HttpContext.Current.Request.Cookies.Add(cookie);
+            var token = HttpContext.Current.Request.Cookies[Config.GetNameCookieOfAccessToken()];
+            if (token?.Value == null) return false;
+            return token.Value != string.Empty;
+        }
+
+        /// <summary>
+        /// Проверяем есть ли token в куках
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsExistRefreshTokenInCookie()
+        {
+            var token = HttpContext.Current.Request.Cookies[Config.GetNameCookieOfRefreshToken()];
+            if (token?.Value == null) return false;
+            return token.Value != string.Empty;
+        }
+
+        public static HttpCookie ReadJwtTokenFromCookie()
+        {
+            return HttpContext.Current.Request.Cookies[Config.GetNameCookieOfAccessToken()];
+        }
+
+        /// <summary>
+        /// Проверка, истек ли срок действия токена
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsExpiredToken()
+        {
+            long expiredSeconds = 0;
+            var payload = GetPayloadFromToken();
+            if (long.TryParse(payload.exp.ToString(), out long value)) expiredSeconds = value;
+            var expired = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).ToUniversalTime().AddSeconds(expiredSeconds);
+            var now = DateTime.Now.ToUniversalTime();
+            return expired < now;
+            //return expired < DateTime.Now.ToUniversalTime();
+        }
+
+        /// <summary>
+        /// При выходе из ЛК удаляем куки содержащий токен
+        /// </summary>
+        public static void Logout()
+        {
+            HttpContext.Current.Response.SetCookie(new HttpCookie(Config.GetNameCookieOfAccessToken()) { Expires = DateTime.Now.AddDays(-1) });
+            HttpContext.Current.Response.SetCookie(new HttpCookie(Config.GetNameCookieOfRefreshToken()) { Expires = DateTime.Now.AddDays(-1) });
+        }
+
+        public static List<string> GetRole()
+        {
+            var role = new List<string>();
+            var payload = GetPayloadFromToken();
+            if (!string.IsNullOrEmpty(payload.role) && !string.IsNullOrEmpty(payload.role.ToString()))
+            {
+                //Сделал так, потому что если у пользователя 1 роль, то DeserializeObject выдает ошибку, почему то не парсит
+                //Подозрение в том, что если роль одна то приходит строка, если ролей много приходит массив
+                try
+                {
+                    role = Newtonsoft.Json.JsonConvert.DeserializeObject<List<string>>(payload.role.ToString());
+                }
+                catch (Exception)
+                {
+                    role = new List<string> { payload.role.ToString() };
+                }
+            }
+            return role;
+        }
+
+        public static string GetUser()
+        {
+            var user = string.Empty;
+            var payload = GetPayloadFromToken();
+            if (!string.IsNullOrEmpty(payload.user.ToString())) user = payload.user.ToString();
+            return user;
+        }
+
+        public static short GetOperator()
+        {
+            short oper = 0;
+            var payload = GetPayloadFromToken();
+            if (short.TryParse(payload.oper.ToString(), out short value)) oper = value;
+            return oper;
+        }
+
+        public static int GetClient()
+        {
+            int client = 0;
+            var payload = GetPayloadFromToken();
+            if (int.TryParse(payload.client.ToString(), out int value)) client = value;
+            return client;
+        }
+
+        public static short GetPartner()
+        {
+            short partner = 0;
+            var payload = GetPayloadFromToken();
+            try
+            {
+                if (short.TryParse(payload.partner.ToString(), out short value)) partner = value;
+            }
+            catch { }
+            return partner;
+        }
+
+        public static short GetPos()
+        {
+            short pos = 0;
+            var payload = GetPayloadFromToken();
+            try
+            {
+                if (short.TryParse(payload.pos.ToString(), out short value)) pos = value;
+            }
+            catch { }
+            return pos;
+        }
+
+
+        public static string GetPosCode()
+        {
+            var posCode = string.Empty;
+            var payload = GetPayloadFromToken();
+            try
+            {
+                if (!string.IsNullOrEmpty(payload.poscode.ToString())) posCode = payload.poscode.ToString();
+            }
+            catch { }
+            return posCode;
+        }
+
+        public static short GetDefaultPartner()
+        {
+            short defaultPartner = 0;
+            var payload = GetPayloadFromToken();
+            try
+            {
+                if (short.TryParse(payload.defaultpartner.ToString(), out short value)) defaultPartner = value;
+            }
+            catch { }
+            return defaultPartner;
+        }
+
+        public static short GetDefaultPos()
+        {
+            short defaultPos = 0;
+            var payload = GetPayloadFromToken();
+            try
+            {
+                if (short.TryParse(payload.defaultpos.ToString(), out short value)) defaultPos = value;
+            }
+            catch { }
+            return defaultPos;
+        }
+
+        public static string GetDefaultPosCode()
+        {
+            var defaultPosCode = string.Empty;
+            var payload = GetPayloadFromToken();
+            try
+            {
+                if (!string.IsNullOrEmpty(payload.defaultposcode.ToString())) defaultPosCode = payload.defaultposcode.ToString();
+            }
+            catch { }
+            return defaultPosCode;
+        }
+
+        public static string GetPermissionCode()
+        {
+            var permission = string.Empty;
+            var payload = GetPayloadFromToken();
+            if (!string.IsNullOrEmpty(payload.permissioncode.ToString())) permission = payload.permissioncode.ToString();
+            return permission;
+        }
+        #endregion
+    }
+}
--- LCManagerClient/LCManager.Site/Controllers/BonusController.cs
+++ LCManagerClient/LCManager.Site/Controllers/BonusController.cs
@@ -1,10 +1,16 @@
-﻿using LC_Manager.Implementation;
+﻿using Site.Infrastrucure.Data;
 using LC_Manager.Models;
+using Site.Infrastrucure.Request;
+using Site.Infrastrucure.Response;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+using System.Globalization;
+using Newtonsoft.Json;
+using LC_Manager.Implementation;
 
 namespace LC_Manager.Controllers
 {
@@ -17,7 +23,7 @@ namespace LC_Manager.Controllers
             try
             {
                 ViewBag.OperatorName = Convert.ToString(HttpContext.Request.Cookies["operatorName"].Value);
-                ViewBag.RoleName = JwtProps.GetRole();
+                ViewBag.RoleName = LC_Manager.Implementation.JwtProps.GetRole();
             }
             catch { }
             return View();
@@ -26,140 +32,87 @@ namespace LC_Manager.Controllers
         [AuthorizeJwt(Roles = "BonusNoChequeList"), HttpPost]
         public string GetBonuses(JQueryDataTableParamModel param)
         {
-            return null;
-            //    ClientInfoResponse response = new ClientInfoResponse();
-            //    try
-            //    {
-            //        ClientInfoRequest request = new ClientInfoRequest
-            //        {
-            //            ClientId = JwtProps.GetClient(),
-            //            OperatorId = JwtProps.GetOperator()
-            //        };
-            //        HttpResponseMessage responseMessage = HttpClientService.PostAsync("api/client/GetClient", request).Result;
-            //        if (responseMessage.IsSuccessStatusCode)
-            //        {
-            //            response = responseMessage.Content.ReadAsAsync<ClientInfoResponse>().Result;
-            //            if (response.ErrorCode == 0)
-            //            {
-            //                BonusesNotForPurchasesResponse response2 = new BonusesNotForPurchasesResponse();
-            //                BonusesNotForPurchasesRequest request2 = new BonusesNotForPurchasesRequest
-            //                {
-            //                    Card = response.ClientData.CardNumber,
-            //                    Date = Request["columns[1][search][value]"],
-            //                    Page = Convert.ToInt64(param.start),
-            //                    PageSize = Convert.ToInt64(param.length)
-            //                };
+            BonusesNotForPurchasesResponse response = new BonusesNotForPurchasesResponse();
+            try
+            {
+                BonusesNotForPurchasesRequest request = new BonusesNotForPurchasesRequest
+                {
+                    Operator = Implementation.JwtProps.GetOperator(),
+                    //Card = response.ClientData.CardNumber,
+                    Date = Request["columns[1][search][value]"],
+                    Page = Convert.ToInt64(param.start),
+                    PageSize = Convert.ToInt64(param.length)
+                };
 
-            //                if (!string.IsNullOrEmpty(Request["columns[2][search][value]"]))
-            //                {
-            //                    request2.Name = Request["columns[2][search][value]"]
-            //                        .Substring(1, Request["columns[2][search][value]"].Length - 2);
-            //                }
-            //                if (!string.IsNullOrEmpty(Request["columns[3][search][value]"]))
-            //                {
-            //                    var values = Request["columns[3][search][value]"].Split('-');
-            //                    try { request2.AddedMore = values[0]; } catch { };
-            //                    try { request2.AddedLess = values[1]; } catch { };
-            //                }
-            //                if (!string.IsNullOrEmpty(Request["columns[4][search][value]"]))
-            //                {
-            //                    var values = Request["columns[4][search][value]"].Split('-');
-            //                    try { request2.RedeemedMore = values[0]; } catch { };
-            //                    try { request2.RedeemedLess = values[1]; } catch { };
-            //                }
-            //                if (!string.IsNullOrEmpty(Request["columns[5][search][value]"]))
-            //                {
-            //                    var values = Request["columns[5][search][value]"].Split('-');
-            //                    try { request2.BurnMore = values[0]; } catch { };
-            //                    try { request2.BurnLess = values[1]; } catch { };
-            //                }
-
-            //                try
-            //                {
-            //                    request2.Page++;
-            //                }
-            //                catch { }
-            //                HttpResponseMessage responseMessage2 = HttpClientService.PostAsync("api/client/BonusesNotForPurchases", request2).Result;
-            //                if (responseMessage2.IsSuccessStatusCode)
-            //                {
-            //                    try
-            //                    {
-            //                        response2 = responseMessage2.Content.ReadAsAsync<BonusesNotForPurchasesResponse>().Result;
-            //                    }
-            //                    catch (Exception e)
-            //                    {
-            //                        var o = e.Message;
-            //                    }
-            //                    if (response2.ErrorCode == 0)
-            //                    {
-            //                        Bonuses bonuses = new Bonuses();
-            //                        foreach (Bonus c in response2.Bonuses)
-            //                        {
-            //                            BonusesViewModel bonus = new BonusesViewModel
-            //                            {
-            //                                date = "<p>" + c.BonusDate.ToString("dd.MM.yyyy") + "</p> <p>" +
-            //                                       c.BonusDate.ToString("HH:mm") + "</p>",
-            //                                type = c.BonusSource,
-            //                                added = c.BonusAdded.ToString(CultureInfo.InvariantCulture),
-            //                                redeemed = c.BonusRedeemed.ToString(CultureInfo.InvariantCulture),
-            //                                fireed = c.BonusBurn.ToString(CultureInfo.InvariantCulture),
-            //                                lorem = ""
-            //                            };
-            //                            bonuses.data.Add(bonus);
-            //                        }
-            //                        bonuses.draw = param.draw;
-            //                        bonuses.recordsTotal = response2.RecordTotal;
-            //                        bonuses.recordsFiltered = response2.RecordFilterd;
-            //                        var data = JsonConvert.SerializeObject(bonuses);
-            //                        return data;
-            //                    }
-            //                    return responseMessage2.ReasonPhrase;
-            //                }
-
-
-            //                #region OldVariant
-            //                //CardBonusesResponse response2 = new CardBonusesResponse();
-            //                //CardBonusesRequest request2 = new CardBonusesRequest
-            //                //{
-            //                //    Card = response.ClientData.CardNumber,
-            //                //    Operator = Convert.ToInt16(Config.GetOperator())
-            //                //};
-            //                //HttpResponseMessage responseMessage2 = HttpClientService.PostAsync("api/client/CardBonuses", request2).Result;
-            //                //if (responseMessage2.IsSuccessStatusCode)
-            //                //{
-            //                //    response2 = responseMessage2.Content.ReadAsAsync<CardBonusesResponse>().Result;
-            //                //    if (response2.ErrorCode == 0)
-            //                //    {
-            //                //        Bonuses bonuses = new Bonuses();
-            //                //        foreach (var c in response2.CardBonuses)
-            //                //        {
-            //                //            BonusesViewModel bonus = new BonusesViewModel
-            //                //            {
-            //                //                date = "<p>" + c.BonusTime.ToString("dd.MM.yyyy") + "</p> <p>" +
-            //                //                       c.BonusTime.ToString("HH:mm") + "</p>",
-            //                //                type = c.BonusType,
-            //                //                added = c.Bonus.ToString(),
-            //                //                lorem = ""
-            //                //            };
-            //                //            bonuses.data.Add(bonus);
-            //                //        }
-            //                //        var data = JsonConvert.SerializeObject(bonuses);
-            //                //        return data;
-            //                //    }
-            //                //    return responseMessage2.ReasonPhrase;
-            //                //} 
-            //                #endregion
-            //            }
-            //        }
-            //        response.ErrorCode = 10;
-            //        response.Message = "Ошибка получения данных";
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        response.ErrorCode = 2;
-            //        response.Message = ex.Message;
-            //    }
-            //    return JsonConvert.SerializeObject(response);
+                if (!string.IsNullOrEmpty(Request["columns[2][search][value]"]))
+                {
+                    request.Name = Request["columns[2][search][value]"]
+                        .Substring(1, Request["columns[2][search][value]"].Length - 2);
+                }
+                if (!string.IsNullOrEmpty(Request["columns[3][search][value]"]))
+                {
+                    var values = Request["columns[3][search][value]"].Split('-');
+                    try { request.AddedMore = values[0]; } catch { };
+                    try { request.AddedLess = values[1]; } catch { };
+                }
+                if (!string.IsNullOrEmpty(Request["columns[4][search][value]"]))
+                {
+                    var values = Request["columns[4][search][value]"].Split('-');
+                    try { request.RedeemedMore = values[0]; } catch { };
+                    try { request.RedeemedLess = values[1]; } catch { };
+                }
+                if (!string.IsNullOrEmpty(Request["columns[5][search][value]"]))
+                {
+                    var values = Request["columns[5][search][value]"].Split('-');
+                    try { request.BurnMore = values[0]; } catch { };
+                    try { request.BurnLess = values[1]; } catch { };
+                }
+                try { request.Page++; } catch { }
+                HttpResponseMessage responseMessage2 = HttpClientService.PostAsync("api/client/BonusesNotForPurchases", request).Result;
+                if (responseMessage2.IsSuccessStatusCode)
+                {
+                    try
+                    {
+                        response = responseMessage2.Content.ReadAsAsync<BonusesNotForPurchasesResponse>().Result;
+                    }
+                    catch (Exception e)
+                    {
+                        var o = e.Message;
+                    }
+                    if (response.ErrorCode == 0)
+                    {
+                        Bonuses bonuses = new Bonuses();
+                        foreach (Site.Infrastrucure.Data.Bonus c in response.Bonuses)
+                        {
+                            BonusesViewModel bonus = new BonusesViewModel
+                            {
+                                date = "<p>" + c.BonusDate.ToString("dd.MM.yyyy") + "</p> <p>" +
+                                       c.BonusDate.ToString("HH:mm") + "</p>",
+                                type = c.BonusSource,
+                                added = c.BonusAdded.ToString(CultureInfo.InvariantCulture),
+                                redeemed = c.BonusRedeemed.ToString(CultureInfo.InvariantCulture),
+                                fireed = c.BonusBurn.ToString(CultureInfo.InvariantCulture),
+                                lorem = ""
+                            };
+                            bonuses.data.Add(bonus);
+                        }
+                        bonuses.draw = param.draw;
+                        bonuses.recordsTotal = response.RecordTotal;
+                        bonuses.recordsFiltered = response.RecordFilterd;
+                        var data = JsonConvert.SerializeObject(bonuses);
+                        return data;
+                    }
+                    return responseMessage2.ReasonPhrase;
+                }
+                response.ErrorCode = 10;
+                response.Message = "Ошибка получения данных";
+            }
+            catch (Exception ex)
+            {
+                response.ErrorCode = 2;
+                response.Message = ex.Message;
+            }
+            return JsonConvert.SerializeObject(response);
         }
     }
 }
