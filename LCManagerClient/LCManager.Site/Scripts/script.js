1423d7270b81a6a6c7eeb7e7006ece38be88b502
YusupovZA <jusupovz@gmail.com>
2018-08-21 23:22:59 +0500

    правка по замечаниям

--- LCManagerClient/LCManager.Site/Scripts/script.js
+++ LCManagerClient/LCManager.Site/Scripts/script.js
@@ -79,7 +79,6 @@ function personData(d) {
     return '<div>'+
             '<div>'+((d.diagram === null)?"":d.diagram)+'</div>'+
             '<div><div class="userlist_info_o">'+
-            '<h3><a href="#" id="additionalInformation" onclick="showBuysModalWindow(this); return false" style="text-decoration: none; color: #58afdd;">Дополнительная информация:</a></h3>'+
 		    '<div class="client_list_ifo_h"><div>?<p>Указано общее количество и сумма покупок участника за всё время участия в программе, включающее в себя покупки, по которым был возврат и списанные бонусы, которыми участник оплатил часть покупки. <br/>'+
 	'Важно!<br/>'+
 	'Покупка - это общая сумма оплаченных денег и бонусов.<br/>' +
@@ -107,31 +106,30 @@ function personData(d) {
             ((d.posRegister === undefined)?"-":(d.posRegister))+'</span>'+
             '<span>'+((d.dateRegister === undefined)?"-":(d.dateRegister))+'</span>'+
             '</div></div>'+
-            '<div class="userlist_info_t"><h3><a href="#" onclick="showBonusesModalWindow(); return false;" style="text-decoration: none; color: #58afdd;">Бонусы не за покупки:</a>' +
-            '<a href="#" onclick="showClientChangeModalWindow(' + d.card +'); return false;" style="text-decoration: none; color: #58afdd; display: none;">Редактирование карточки клиента</a></h3>'+
-            '<div class="client_list_ifo_h"><div>?<p>123123</p></div><p>Welcome: </p><span>'+
-            ((d.welcomeBonusDate === undefined)?"-":(d.welcomeBonusDate))+'</span>'+
-            '<span>'+((d.welcomeBonusAmount === undefined)?"-":(d.welcomeBonusAmount+' б.'))+'</span>'+
+            '<div class="userlist_info_t">' +
+            '<div class="client_list_ifo_h"><div>?<p>123123</p></div><p>Деньги в кассу: </p><span>'+
+            ((d.welcomeBonusDate1 === undefined)?"-":(d.welcomeBonusDate1))+'</span>'+
+            '<span>'+((d.welcomeBonusAmount1 === undefined)?"-":(d.welcomeBonusAmount1+' б.'))+'</span>'+
             '</div>'+
-            '<div class="client_list_ifo_h"><div>?<p>123123</p></div><p>Promo: </p><span>'+
+            '<div class="client_list_ifo_h"><div>?<p>123123</p></div><p>Бонусы не за покупки: </p><span>'+
             ((d.promoBonusDate === undefined)?"-":(d.promoBonusDate))+'</span>'+
             '<span>'+((d.promoBonusAmount === undefined)?"-":(d.promoBonusAmount+' б.'))+'</span>'+
             '</div>'+
-            '<div class="client_list_ifo_h"><div>?<p>123123</p></div><p>Operator: </p><span>'+
+            '<div class="client_list_ifo_h"><div>?<p>123123</p></div><p>Друзей привёл: </p><span>'+
             ((d.opperatorBonusDate === undefined)?"-":(d.opperatorBonusDate))+'</span>'+
             '<span>'+((d.operatorBonusAmount === undefined)?"-":(d.operatorBonusAmount+' б.'))+'</span>'+
             '</div>'+
-            '<div class="client_list_ifo_h"><div>?<p>123123</p></div><p>Friend: </p><span>'+
+            '<div class="client_list_ifo_h"><div>?<p>123123</p></div><p>Статус клиента: </p><span>'+
             ((d.friendBonusDate === undefined)?"-":(d.friendBonusDate))+'</span>'+
             '<span>'+((d.friendBonusAmount === undefined)?"-":(d.friendBonusAmount+' б.'))+'</span>'+
             '</div>'+
-            '<div class="client_list_ifo_h"><div>?<p>123123</p></div><p>Birthday: </p><span>'+
+            '<div class="client_list_ifo_h"><div>?<p>123123</p></div><p>Статус карты: </p><span>'+
             ((d.birthdayBonusDate === undefined)?"-":(d.birthdayBonusDate))+'</span>'+
             '<span>'+((d.birthdayBonusAmount === undefined)?"-":(d.birthdayBonusAmount+' б.'))+'</span>'+
             '</div></div><div class="userlist_info_actions">' +
             '<a href="#" onclick="showClientChangeModalWindow(' + d.card +')" class="actions">Профиль</a>'+
-            '<a href="#" class="actions">Покупки</a>'+
-            '<a href="#" class="actions">Бонусы</a>' +
+            '<a href="#" class="actions" onclick="showBuysModalWindow(this); return false">Покупки</a>'+
+            '<a href="#" class="actions" onclick="showBonusesModalWindow(); return false;">Бонусы</a>' +
             '<a href="#" class="actions">Маркетинг</a>' +
             '<a href="#" class="actions">Корзина</a>'
             + '</div></div>' +
